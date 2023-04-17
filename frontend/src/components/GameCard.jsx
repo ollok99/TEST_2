2b7f9cbee7a5877cd09c1646f1f6209458eb8422
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-18 00:18:28 +0900

    mainpage_test

---
+++ frontend/src/components/GameCard.jsx
@@ -0,0 +1,16 @@
+// import GameStart from "../../public/images/gamestart.png";
+
+const GameCard = () => {
+  return (
+    <form className="bg-yellow-500 flex flex-col justify-end rounded-xl w-120 h-120 mx-2">
+      <div className="w-full h-4/5">
+        <img src="" alt="게임시작이미지"></img>
+      </div>
+      <div className="bg-red-100 w-full rounded-b-xl text-xs pb-0.5 pl-1">
+        title
+      </div>
+    </form>
+  );
+};
+
+export default GameCard;
---
+++ frontend/src/components/NameCard.jsx
@@ -0,0 +1,23 @@
+const NameCard = () => {
+  return (
+    <>
+      <button className="bg-purple-100 px-2 py-1 mx-4 text-white rounded-xl bg-opacity-40">
+        현진
+      </button>
+      <button className="bg-purple-100 px-2 py-1 mx-4 text-white rounded-xl bg-opacity-40">
+        주연
+      </button>
+      <button className="bg-purple-100 px-2 py-1 mx-4 text-white rounded-xl bg-opacity-40">
+        성윤
+      </button>
+      <button className="bg-purple-100 px-2 py-1 mx-4 text-white rounded-xl bg-opacity-40">
+        규혁
+      </button>
+      <button className="bg-purple-100 px-2 py-1 mx-4 text-white rounded-xl bg-opacity-40">
+        범진
+      </button>
+    </>
+  );
+};
+
+export default NameCard;
